3fbcd09549c1b6565aa39c434fdab43cca54bb68
Калажокова Лиана <lizakalazhokova@edu.hse.ru>
2026-06-12 00:57:13 +0300

    Реализован Dockerfile.reporter. Добавлены новые функции в run.sh.

--- run.sh
+++ run.sh
@@ -9,13 +9,25 @@ build_generator() {
 run_generator() {
     echo "Запуск генератора..."
     docker run --rm -v "$(pwd)/data:/data" generate-image
-    echo "Cоздан файл data.csv в data/"
+    echo "Создан файл data.csv в data/"
 }
 
 create_local_data() {
     echo "Локальная генерация..."
     python generate.py local_data
     echo "Файл data.csv создан"
+}
+
+build_reporter() {
+    echo "Сборка образа для контейнера аналитика..."
+    docker build -f Dockerfile.reporter -t reporter-image .
+    echo "Образ создан"
+}
+
+run_reporter() {
+    echo "Запуск контейнера для генерации отчета..."
+    docker run --rm -v "$(pwd)/data:/data" reporter-image
+    echo "Отчет создан"
 }
 
 case "$1" in
@@ -27,6 +39,12 @@ case "$1" in
         ;;
     create_local_data)
         create_local_data
+        ;;
+    build_reporter)
+        build_reporter
+        ;;
+    run_reporter)
+        run_reporter
         ;;
     *)
         echo "Запуск функции из run.sh..."
